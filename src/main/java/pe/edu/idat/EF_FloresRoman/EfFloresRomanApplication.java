7fee3ac884a565f7efed482111b027a5cad3dc16
JesusFlores0199 <a20204669@idat.pe>
2025-04-02 09:43:18 -0500

    initial commit

---
+++ src/main/java/pe/edu/idat/EF_FloresRoman/EfFloresRomanApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.idat.EF_FloresRoman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EfFloresRomanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EfFloresRomanApplication.class, args);
+	}
+
+}
